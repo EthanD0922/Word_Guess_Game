b198246d8898f3bfad7eb143237147bcd76c3b8e
Ethan <allric0922@gmail.com>
2019-07-16 18:40:47 -0400

    logic works, except for 1st correct answer changes guess attempts others dont

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,102 @@
-// var questions =[
-//     "eleven",
-//     "dustin",
-//     "mike",
-//     "lucas",
-//     "will",
-//     "nancy",
-//     "hopper",
-//     "steve",
-//     "max",
-//     "jonathan",
-//     "joyce",
-//     "erica",
-//     "robin",
-// ];
-
-// var activeQuestion = questions[Math.floor(Math.random()*questions.length)]
-// // var starLength = 0
-// // var characters = document.getElementById("character")
-//         // for (i=0; i< activeQuestion.length + 1; i++){
-//         //     var blanks = document.createElement("div");
-//         //     blanks.textContent = questions[i];
-//         //     characters.appendChild(blanks);
-//         // }
-
-
-var drinkList = [
-    "Coffee: $5",
-    "Espresso: $7",
-    "Cappuccino: $6",
-    "Latte: $4",
-    "Tea: $3",
-    "Ice Coffee: $6",
-    "Ice Espresso: $8",
-    "Ice Latte: $6",
-    "Ice Tea: $4"
+var questions =[
+    "max",
+    "mike",
+    "will",
+    "joyce",
+    "erica",
+    "robin",
+    "lucas",
+    "nancy",
+    "steve",
+    "hopper",
+    "eleven",
+    "dustin",
+    "jonathan",  
 ];
 
-var targetDiv = document.getElementById("drink-options")
-for (var i = 0; i < drinkList.length; i++) {
-    var babyDiv = document.createElement("div");
-    babyDiv.textContent = drinkList[i];
-    targetDiv.appendChild(babyDiv);
+
+var wins = "0"
+
+var x = 0
+function gameOver(){
+    document.querySelector("#winsCount").textContent = "You contacted: " + wins + " members!";
+    document.querySelector("#fails").textContent = "";
+    document.querySelector("#guesses").textContent = " ";
+    document.querySelector("#character").textContent = "";
 }
+
+function start(){
+var dashes =[
+
+];
+
+var userGuessed = [ 
+
+];
+var attemptsLeft = 10
+document.querySelector("#winsCount").textContent = wins;
+document.querySelector("#fails").textContent = attemptsLeft;
+
+var activeQuestion = questions[x]
+var wordArray = activeQuestion.split("")
+var length = 0
+var character = document.getElementById("character")
+   
+
+    for (i=0; i< activeQuestion.length; i++) {
+        var newDashP = document.createElement("p");
+        dashes[i] = "-"
+        character.appendChild(newDashP)
+    
+    }
+    newDashP.textContent = dashes
+    document.onkeyup = function test(event) {
+       
+         var letter = event.key.toLowerCase();
+        if (event.keyCode >= 65 &&  event.keyCode <= 90){
+            
+            for (p=0;p < userGuessed.length; p++){
+                if (letter === userGuessed[p]){
+                    console.log("same")
+                    return;
+                }
+            
+            }
+            attemptsLeft --
+            document.querySelector("#fails").textContent = attemptsLeft;
+                userGuessed[userGuessed.length] = letter
+            if (attemptsLeft <= 0){
+                gameOver()
+            }
+                
+            document.querySelector("#guesses").textContent = userGuessed;
+                if (userGuessed.indexOf(letter) === (userGuessed.length -1)){
+                    for (i=0; i< activeQuestion.length; i++){
+                    
+                        if (letter === wordArray[i]){
+                            length ++
+                            attemptsLeft ++     
+                            dashes[i] = letter
+                            newDashP.textContent = dashes
+                            }
+                        
+                        // console.log(length)
+                        if (length === activeQuestion.length){
+                            document.querySelector("#guesses").textContent = "";
+                            wins ++
+                            x ++
+                            newDashP.textContent = ""
+                            if (x === questions.length - 1){
+                                gameOver()
+                            }
+                            start();
+                            }
+                    }
+                
+                
+                }
+        }
+    }  
+}
+
+start()
